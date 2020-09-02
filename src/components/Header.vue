b8213457b969f3305e09d6e2c85ec5e43589b2c7
CeoFred <johnsonmessilo19@gmail.com>
2020-09-02 09:44:15 +0100

    oka

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+ <ul class="nav">
+ 
+   <router-link exact to="/" tag="li" active-class="active"><a>Home</a></router-link>
+    <router-link exact to="/user" tag="li" active-class="active"><a>User</a></router-link>
+ 
+</ul>
+</template>
---
+++ src/components/user/UserDetail.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+    <h3>Some User Details</h3>
+<p>User loaded has ID: {{ $route.params.id }}</p>
+
+<router-link :to="{ name:'userEdit', params: { id: $route.params.id } }" class="btn btn-primary" tag="button">Edit User</router-link>
+</div>
+</template>
+
